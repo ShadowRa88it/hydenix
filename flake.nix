db0b5cea3f9fb0f7640f97a6895d52f48f1c7945
shadowra88it <shadowra88it@gmail.com>
2024-12-29 22:37:41 -1000

    added outputs and removed default hydenixConfig

--- flake.nix
+++ flake.nix
@@ -16,16 +16,17 @@
   outputs =
     {
       hydenix,
+      nixpkgs,
       ...
     }@inputs:
     let
       system = "x86_64-linux";
 
-      hydenixConfig = hydenix.lib.mkConfig {
-        userConfig = import ./config.nix;
-        # inputs without nixpkgs to prevent override
-        extraInputs = removeAttrs inputs [ "nixpkgs" ];
-      };
+      # hydenixConfig = hydenix.lib.mkConfig {
+      #   userConfig = import ./config.nix;
+      #   # inputs without nixpkgs to prevent override
+      #   extraInputs = removeAttrs inputs [ "nixpkgs" ];
+      # };
     in
     {
 
